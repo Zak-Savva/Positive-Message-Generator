8c702cb42fce831909ff49b49e26a4bef21ec5ab
Zach <zak.savva@hotmail.co.uk>
2021-02-28 14:37:20 +0000

    Add arrays and a test for genny

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = 'Zach';
+const prefix = ['the greatest', 'the mostest', 'the biggest', 'the hugest', 'the el chungo grandeist', 'the kingeyest', 'the queenyest'];
+const adjective = ['most adorable', 'most attractive', 'most beautiful', 'most cute', 'most elegent', 'most gorgeous'];
+const adjective2 = ['likeable', 'witty', 'sensible', 'trustworthy', 'sympathetic', 'generous', 'considerate'];
+const descriptor = ['human', 'person', 'bastard', 'individual', 'fellow', 'chicken', 'dude', 'sod', 'chap(ette)'];
+
+console.log(`Hello ${name}, you are ${prefix[2]}, ${adjective[4]} and ${adjective2[0]} ${descriptor[6]} of all time :)`)
